61f378740e728ea6ace4e0adc109bb61f50c29a6
Shivam11-g <54078532+Shivam11-g@users.noreply.github.com>
2020-06-19 22:08:28 +0530

    Updates

---
+++ precache-manifest.28d5b760075db80755aa56c4a7059265.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc094cc4496fb34cbf8526a9af9b4798",
+    "url": "/covid19-center/index.html"
+  },
+  {
+    "revision": "095a8a2b039479299691",
+    "url": "/covid19-center/static/css/main.5e22151d.chunk.css"
+  },
+  {
+    "revision": "d1413c15abc0fa9b60c2",
+    "url": "/covid19-center/static/js/2.5874bf93.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/covid19-center/static/js/2.5874bf93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "095a8a2b039479299691",
+    "url": "/covid19-center/static/js/main.8b26d66e.chunk.js"
+  },
+  {
+    "revision": "bb3815b24c97ae8d8400",
+    "url": "/covid19-center/static/js/runtime-main.4df654eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-center/static/media/logo.5d5d9eef.svg"
+  }
+]);
